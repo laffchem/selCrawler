05de3b68a078f7653a73e83b127e526f3c794ca3
John Laffey <laffeychem@gmail.com>
2024-05-13 16:28:12 -0400

    add link gathering functionality

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-import { instantiateDriver, goHome } from './logic.js';
+import {
+    instantiateDriver,
+    goHome,
+    getCategoryLinks,
+    getProductLinks,
+} from './logic.js';
 
 // Necessary variables
 const links = new Set();
 
 const driver = await instantiateDriver();
 await goHome(driver);
+await getCategoryLinks(driver);
+await driver.get('https://www.flexshopper.com/dp/apple');
+await getProductLinks(driver);
+await driver.quit();
--- logic.js
+++ logic.js
@@ -4,15 +4,39 @@ import { sleep } from './utils.js';
 
 export async function instantiateDriver() {
     const driver = new Builder().forBrowser('chrome').build();
+    await driver.manage().setTimeouts({ implicit: 120000 }); // Note this timeout is due to some brand pages taking long times to load. -- Samsung being one of the main ones.
+
     return driver;
 }
 
 export async function goHome(driver) {
     await driver.get('https://flexshopper.com');
-    await sleep(10000);
-    await driver.quit();
 }
 
-export async function scrapeCategories(driver) {
-    const links = await driver.findElement(By.css('a'));
+export async function getCategoryLinks(driver) {
+    const links = await driver.findElements(By.css('a'));
+    const categoryLinks = await Promise.all(
+        links.map(async (link) => {
+            return await link.getAttribute('href');
+        })
+    );
+    const filteredLinks = categoryLinks.filter((link) =>
+        link.includes('/category')
+    );
+    console.log(filteredLinks);
+    return filteredLinks;
+}
+
+export async function getProductLinks(driver) {
+    const links = await driver.findElements(By.css('a'));
+    const categoryLinks = await Promise.all(
+        links.map(async (link) => {
+            return await link.getAttribute('href');
+        })
+    );
+    const filteredLinks = categoryLinks.filter((link) =>
+        link.includes('/product')
+    );
+    console.log(filteredLinks);
+    return filteredLinks;
 }
